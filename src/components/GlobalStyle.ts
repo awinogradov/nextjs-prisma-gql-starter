7da120729b2ce5db5faecd2abd07f1d6e5b5b282
Anton Vinogradov <winogradovaa@gmail.com>
2022-03-26 21:49:08 +0300

    themes support

--- src/components/GlobalStyle.ts
+++ src/components/GlobalStyle.ts
@@ -1,9 +1,12 @@
 import { createGlobalStyle } from 'styled-components';
+import { backgroundColor } from '../design/@generated/themes';
 
 export const GlobalStyle = createGlobalStyle`
     body {
         margin: 0;
         padding: 0;
         box-sizing: border-box;
+
+        background-color: {backgroundColor};
     }
 `;
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,45 +2,66 @@ import { useEffect } from 'react';
 import type { AppProps } from 'next/app';
 import { ApolloProvider } from '@apollo/client';
 import { SessionProvider, useSession, signIn } from 'next-auth/react';
+import { ThemeProvider } from 'next-themes';
+import { useTheme } from 'next-themes';
 
-import { GlobalStyle } from '../components/GlobalStyle';
+import { Theme } from '../components/Theme';
 import { apolloClient } from '../utils/apolloClient';
+import { GlobalStyle } from '../components/GlobalStyle';
+import { ThemeChanger } from '../components/ThemeChanger';
 import { NextPageWithAuthAuth } from '../types/nextPageWithAuth';
 
 type AppPropsWithAuth = AppProps & {
     Component: NextPageWithAuthAuth;
 };
 
-function Auth({ children }: { children: React.ReactNode }) {
+const Auth = ({ children }: { children: React.ReactNode }) => {
     const { data: session, status } = useSession();
 
     const isUser = !!session?.user;
+
     useEffect(() => {
         if (status === 'loading') return;
         if (!isUser) signIn();
     }, [isUser, status]);
 
     return isUser ? <>{children}</> : null;
-}
+};
 
-function App({ Component, pageProps: { session, ...pageProps } }: AppPropsWithAuth) {
+const Root = ({ Component, pageProps }: { Component: NextPageWithAuthAuth; pageProps: any }) => {
+    const { theme } = useTheme();
+
+    return (
+        <>
+            <Theme theme={theme || 'dark'} />
+
+            <ThemeChanger />
+
+            {Component.auth ? (
+                <Auth>
+                    <Component {...pageProps} />
+                </Auth>
+            ) : (
+                <Component {...pageProps} />
+            )}
+        </>
+    );
+};
+
+const App = ({ Component, pageProps: { session, ...pageProps } }: AppPropsWithAuth) => {
     return (
         <>
             <GlobalStyle />
 
             <SessionProvider session={session}>
                 <ApolloProvider client={apolloClient}>
-                    {Component.auth ? (
-                        <Auth>
-                            <Component {...pageProps} />
-                        </Auth>
-                    ) : (
-                        <Component {...pageProps} />
-                    )}
+                    <ThemeProvider themes={['light', 'dark']} defaultTheme="dark">
+                        <Root Component={Component} pageProps={pageProps} />
+                    </ThemeProvider>
                 </ApolloProvider>
             </SessionProvider>
         </>
     );
-}
+};
 
 export default App;
